54868f87b7ce6ecfefd01b206a851b1a2ddea839
angie10 <delacruzvargasangelica26@gmail.com>
2022-10-31 10:12:20 -0400

    css linked to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../home.css'
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
-		<>
-			<h2>Home Page</h2>
-		</>
+		<body>
+		<div className='title'>
+			<span>T</span>
+			<span>E</span>
+			<span>S</span>
+			<span>T</span>
+			
+		</div>
+		<div className='buttons'>
+		    <Link className='btn btn-success' to='sign-up'>Sign-Up</Link>
+			
+			<Link className='btn btn-success' to='sign-in'>Sign-In</Link>
+		</div>
+		</body>
 	)
 }
 
