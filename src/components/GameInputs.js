df12c3a58f9fc6424a5c0d8686368977cd801483
saparker <stephanieparker1997@gmail.com>
2022-11-01 19:14:19 -0400

    fixed GameInput

--- src/components/GameInputs.js
+++ src/components/GameInputs.js
@@ -32,145 +32,69 @@ const difficulties = ["Easy", "Medium", "Hard"];
 
 const amounts = ["5", "10", "15", "20"];
 
-    const handleDifficultyChange = (e) => {
-        setFilterOptions({
-           difficulty: e.target.value,
-           category,
-           amount
-         })
-      }
-    const handleCategoryChange = (e) =>{
-        console.log(categories.findIndex(category => category === e.target.value) + 8)
-        setFilterOptions({
-            difficulty,
-            category:categories.findIndex(category => category === e.target.value) + 8,
-            amount
-          })
+const GameInputs = ({ setFilterOptions }) => {
+  const [difficulty, setDifficulty] = useState(difficulties[0]);
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState(amounts[0]);
 
-    }
+  const handleDifficultyChange = (e) => {
+    setFilterOptions({
+      difficulty: e.target.value,
+      category,
+      amount,
+    });
+  };
+  const handleCategoryChange = (e) => {
+    console.log(
+      categories.findIndex((category) => category === e.target.value) + 8
+    );
+    setFilterOptions({
+      difficulty,
+      category:
+        categories.findIndex((category) => category === e.target.value) + 8,
+      amount,
+    });
+  };
 
-    const handleAmountChange = (e) =>{
-        setFilterOptions({
-            difficulty,
-            category,
-            amount: Number.parseInt(e.target.value),
-          })
-    }
-   
-   return(
+  const handleAmountChange = (e) => {
+    setFilterOptions({
+      difficulty,
+      category,
+      amount: Number.parseInt(e.target.value),
+    });
+  };
+
+  return (
     <>
-    <form>
+      <form>
         <div className="dropdown">
-        <select value={category} onChange={handleCategoryChange}>
-           { categories.map((category, idx) =>(
-                <option value={category} key={idx}>{category}</option>
+          <select value={category} onChange={handleCategoryChange}>
+            {categories.map((category, idx) => (
+              <option value={category} key={idx}>
+                {category}
+              </option>
             ))}
-           </select>
+          </select>
         </div>
 
         <div className="dropdown">
-          <button
-            className="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton1"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Choose Difficulty
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li>
-              <button
-                className="dropdown-item"
-                type="radio"
-                name="difficulty"
-                onClick={settingChoice}
-                value="Easy"
-              >
-                Easy
-              </button>
-            </li>
-            <li>
-              <button
-                className="dropdown-item"
-                type="radio"
-                name="difficulty"
-                onClick={settingChoice}
-                value="Medium"
-              >
-                Medium
-              </button>
-            </li>
-            <li>
-              <button
-                className="dropdown-item"
-                type="radio"
-                name="difficulty"
-                onClick={settingChoice}
-                value="Hard"
-              >
-                Hard
-              </button>
-            </li>
-          </ul>
+          <select value={difficulty} onChange={handleDifficultyChange}>
+            {difficulties.map((difficulty, idx) => (
+              <option value={difficulty} key={idx}>
+                {difficulty}
+              </option>
+            ))}
+          </select>
         </div>
 
         <div className="dropdown">
-          <button
-            className="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton1"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            # of Questions
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li>
-              <button
-                className="dropdown-item"
-                type="radio"
-                name="number"
-                onClick={settingChoice}
-                value="5"
-              >
-                5
-              </button>
-            </li>
-            <li>
-              <button
-                className="dropdown-item"
-                type="radio"
-                name="number"
-                onClick={settingChoice}
-                value="5"
-              >
-                10
-              </button>
-            </li>
-            <li>
-              <button
-                className="dropdown-item"
-                type="radio"
-                name="number"
-                onClick={settingChoice}
-                value="5"
-              >
-                15
-              </button>
-            </li>
-            <li>
-              <button
-                className="dropdown-item"
-                type="radio"
-                name="number"
-                onClick={settingChoice}
-                value="5"
-              >
-                20
-              </button>
-            </li>
-          </ul>
+          <select value={amount} onChange={handleAmountChange}>
+            {amounts.map((amount, idx) => (
+              <option value={amount} key={idx}>
+                {amount}
+              </option>
+            ))}
+          </select>
         </div>
       </form>
     </>
