29a7f03944873f05a6cb701c661dd276e0040aac
harrisimon <harrisimon@gmail.com>
2022-11-04 09:08:09 -0400

    added edit game modal

---
+++ src/components/GameTitleEditModal.js
@@ -0,0 +1,10 @@
+import React,  { useState } from "react";
+import { Button, Modal } from 'react-bootstrap'
+import { propTypes } from "react-bootstrap/esm/Image";
+import { gameUpdate } from "../api/game";
+
+const GameTitleEditModal = (props) => {
+    const { use, handleClose, msgAlert, triggerRefresh, game } = props
+
+    
+}
