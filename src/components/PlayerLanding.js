f5d9e554787cbafaf86d7c9759361ceb8cfa6599
angie10 <delacruzvargasangelica26@gmail.com>
2022-11-03 20:55:31 -0400

    create game route on landing page

--- src/components/PlayerLanding.js
+++ src/components/PlayerLanding.js
@@ -12,6 +12,11 @@ const PlayerLanding = () => {
             <div style={{ backgroundColor: '#240046', width: '200px', height: '200px', padding: '30px', border: '4px solid #7552f2', marginLeft: '20px', marginTop: '20px', borderRadius: '10px' }}>
                 <p style={{color: 'white'}}>LeaderBoard</p>
             </div>
+
+            <div style={{ backgroundColor: '#240046', width: '200px', height: '200px', padding: '30px', border: '4px solid #7552f2', marginLeft: '20px', marginTop: '20px', borderRadius: '10px' }}>
+            <p style={{color: 'white'}}>Create Your Own Game</p>
+            <Link className='btn' to='/create-game' style={{color: 'white', border:'2px solid #ffc300', fontSize: '15px', borderRadius: '10px'}}>Create Game</Link>
+            </div>
         </div>
     )
 }
