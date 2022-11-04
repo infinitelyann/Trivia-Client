d075a995724a79e48dbf7d29ffdada634830a938
saparker <stephanieparker1997@gmail.com>
2022-11-03 21:53:04 -0400

    correct answers are an array, onClick pushes stringfied value into array

--- src/components/trivia-api/GameCarousel.js
+++ src/components/trivia-api/GameCarousel.js
@@ -4,38 +4,50 @@ import { Card } from "react-bootstrap";
 
 const GameCarousel = (props) =>{
   const { data } = props
-  const { correct_answer, incorrect_answers, question} = data
   const [answer, setAnswer] = useState('')
   const [show, setShow] = useState(true)
+  const correct = []
+  const ansersArr = []
 
   const handleClick = (e) =>{
-    console.log(e.target.innerText)
+    setAnswer((e.target.innerText).toString())
     
-    setAnswer(e.target.innerText)
+    
+    console.log(ansersArr)
   }
+  ansersArr.push(answer)
+  
+ correct.filter()
 
 
   return (
     <>
     
     {data.map((question, index) => {
-      console.log({incorrect_answers})
-      const { incorrect_answers, correct_answer } = question;
+    
+    const { incorrect_answers, correct_answer } = question;
+    correct.push({ correct_answer }) 
+ 
+   
+
       const renderedAnswers = [...incorrect_answers, correct_answer]
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value);
         return(
-         <Card className={index}>
-          <Card.Header>Question: {index + 1}</Card.Header>
-          <div key={index}>
+         <Card key={index} className={index}>
+          <Card.Header>Question: {index + 1} </Card.Header>
+          <div >
             <p dangerouslySetInnerHTML={{__html: question.question}}></p>
 
             {renderedAnswers.map((answer, idx) => (
-              <p dangerouslySetInnerHTML={{__html: answer}}className="btn btn-outline-dark" key={idx} onClick={handleClick}>
+              <p dangerouslySetInnerHTML={{__html: answer}}className="btn btn-outline-dark" key={idx} onClick={handleClick} value={{answer}}>
                 
               </p>
             ))}
+            {/* {correct.filter(e => 
+                
+              )} */}
 
           
           </div>
