3869635d35867194bcd1aec160d27fcc539abf1d
saparker <stephanieparker1997@gmail.com>
2022-11-04 22:09:51 -0400

    refactored game questions from api- user score/game index/ one at a time show

--- src/components/trivia-api/GameCarousel.js
+++ src/components/trivia-api/GameCarousel.js
@@ -1,61 +1,92 @@
-import { useState } from "react";
-import { Card } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Card } from "react-bootstrap"; 
 
 
-const GameCarousel = (props) =>{
-  const { data } = props
-  const [answer, setAnswer] = useState('')
-  const [show, setShow] = useState(true)
-  const correct = []
-  const ansersArr = []
-
-  const handleClick = (e) =>{
-    setAnswer((e.target.innerText).toString())
-    
-    
-    console.log(ansersArr)
-  }
-  ansersArr.push(answer)
+const GameCarousel = (props) => {
+  const { data } = props;
+  const correct = [];
+  const [answer, setAnswer] = useState("");
+  const [correctAns, setCorrectAns] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [questionIndex, setQuestionIndex]  = useState(0)
+  let [userScore, setUserScore] = useState(0)
   
-//   }, [isCorrect, answer, correctAns, userScore])
+  for(let i = 0; i < data.length; i++){
+    correct.push(data[i].correct_answer)
+  }
+  console.log(correct)
+  
+  useEffect(() =>{
+    setAnswer(answer)
+    setIsCorrect(() => {
+      if (answer == correctAns) {
+      
+        return true;
+      } else {
+       
+        return false;
+      }
+    }
+    );
+   
+    setCorrectAns(() => {
+     
+      return correct[questionIndex].correct_answer;
+    })
+
+  }, [isCorrect, answer, correctAns, userScore, setUserScore])
+  const handleClick = (e, idx) => {
+  
+    console.log(e.target);
+    idx = e.target.id;
+    setAnswer(() => {
+      return e.target.innerText;
+    });
+ ;
+    setUserScore(() =>{
+      if(isCorrect){
+        return(userScore + 1)
+      }else{
+        return(userScore - 1)
+      }
+    })
+    setQuestionIndex(()=>{
+      return(questionIndex + 1)
+    })
+
+  
+  }
+
+
+  const renderedAnswers = [...data[questionIndex].incorrect_answers, data[questionIndex].correct_answer];
   
 
   return (
     <>
-    
-    {data.map((question, index) => {
-    
-    const { incorrect_answers, correct_answer } = question;
-    correct.push({ correct_answer }) 
  
-   
-
-      const renderedAnswers = [...incorrect_answers, correct_answer]
-        .map((value) => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value);
-        return(
-         <Card key={index} className={index}>
-          <Card.Header>Question: {index + 1} </Card.Header>
-          <div >
-            <p dangerouslySetInnerHTML={{__html: question.question}}></p>
-
-            {renderedAnswers.map((answer, idx) => (
-              <p dangerouslySetInnerHTML={{__html: answer}}className="btn btn-outline-dark" key={idx} onClick={handleClick} value={{answer}}>
-                
-              </p>
-            ))}
-            {/* {correct.filter(e => 
-                
-              )} */}
-
-          
-          </div>
-         </Card>
-        )
-        
-    })} 
+   <h1> UserScore: {userScore}</h1>
+      <Card>
+      Question: {questionIndex}
+        <Card.Header>
+        {data[questionIndex].question}
+        </Card.Header>
+        <Card.Body>
+        {renderedAnswers.map((answer, idx) => (
+                <p
+                  onClick={handleClick}
+                  id={questionIndex}
+                  className="btn btn-outline-dark"
+                  key={idx}
+                  value={answer}
+                >
+                  {answer}
+                </p>
+              ))}
+        </Card.Body>
+        <button onClick={handleClick}>next ?</button>
+      </Card>
     </>
-  )
-}
-export default GameCarousel 
+  );
+};
+
+export default GameCarousel;
